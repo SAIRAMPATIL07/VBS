8e93a3aa071d1751d43f76a9a5f04a39a9fffaaf
SAIRAMPATIL07 <gtdwot@gmail.com>
2025-12-31 07:48:39 +0530

    Third Commit

--- demo/src/main/java/com/vbs/demo/VbsApplication.java
+++ demo/src/main/java/com/vbs/demo/VbsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VbsApplication {
+public class VbsApplication { //
 
 	public static void main(String[] args) {
 		SpringApplication.run(VbsApplication.class, args);
